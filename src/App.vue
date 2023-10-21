8491e593e49cfe0def4c737e2b9b04ae71fb23b4
Lawrence Almeida <lawbraun.almeida@gmail.com>
2023-10-21 13:32:26 +0100

    Removes elements from scaffolded project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="./assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
-    </header>
-
-    <main>
-      <TheWelcome />
-    </main>
-  </div>
+  <main>
+    <div>Foobar</div>
+  </main>
 </template>
 
 <style scoped>
